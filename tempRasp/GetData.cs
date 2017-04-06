4ae6aeca18e89e35c0f2f4085fcdf00285aae90e
rnieva <roberto.nieva@gmail.com>
2017-04-06 12:03:45 +0100

    First UpLoad

---
+++ tempRasp/GetData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace tempRasp
+{
+
+    public class ModelList
+    {
+        public List<string> dateList = new List<string>();
+        public List<string> timeList = new List<string>();
+        public List<string> sensorList = new List<string>();
+        public List<string> temperatureList = new List<string>();
+    }
+
+    public class GetData
+    {
+        public ModelList getTemp()
+        {
+            ModelList list = new ModelList();
+            try
+            {
+                string connectionString = "Server=192.168.1.19;Database=tempSensor1;Port=3306;User ID=root4;Password=1234";  // credentials
+                MySqlConnection conDB = new MySqlConnection(connectionString);
+                conDB.Open();
+                Console.WriteLine("Connect");
+                string query = "SELECT * FROM dataSensor1";
+                MySqlCommand cmd = new MySqlCommand(query, conDB);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    list.dateList.Add(dataReader["tdate"] + "");
+                    list.timeList.Add(dataReader["ttime"] + "");
+                    list.sensorList.Add(dataReader["sensor"] + "");
+                    list.temperatureList.Add(dataReader["temperature"] + "");
+                }
+                dataReader.Close();
+                conDB.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return list;
+        }
+    }
+}
---
+++ tempRasp/showTemp.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tempRasp
+{
+    public partial class showTemp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetData obj = new GetData();
+            ModelList list = new ModelList();
+            list = obj.getTemp();
+            DataTable dataGrid = new DataTable();
+            dataGrid.Columns.Add("Date");
+            dataGrid.Columns.Add("Time");
+            dataGrid.Columns.Add("Sensor");
+            dataGrid.Columns.Add("Temperature");
+            for (int i = 0; i < list.dateList.Count; i++)
+            {
+                dataGrid.Rows.Add(list.dateList[i], list.timeList[i], list.sensorList[i], list.temperatureList[i]);
+            }
+            GridView1.DataSource = dataGrid;
+            GridView1.DataBind();
+        }
+    }
+}
